4c91754ce4c015f99bdaea6ef9f6ae878d1206bf
Dino Ratcliffe <yo@dino.io>
2015-11-04 16:55:17 +0000

    prototype player movement

---
+++ time-fighters/Assets/Controls.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+	public float maxSpeed = 5F;
+	public float moveForce = 365F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		float hInput = Input.GetAxis ("Horizontal");
+
+		if (hInput * GetComponent<Rigidbody> ().velocity.x < maxSpeed) {
+			GetComponent<Rigidbody>().AddForce (Vector2.right * hInput * moveForce);
+		}
+	}
+}
