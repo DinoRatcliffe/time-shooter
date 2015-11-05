3353eb86a2033c331a2436c349cf2617e7af8898
Dino Ratcliffe <yo@dino.io>
2015-11-05 16:52:23 +0000

    added wall, prevent wall hanging

--- time-fighters/Assets/Controls.cs
+++ time-fighters/Assets/Controls.cs
@@ -5,19 +5,21 @@ public class Controls : MonoBehaviour {
 	public float maxSpeed = 5F;
 	public float moveForce = 365F;
 	public float jumpForce = 300F;
-	Transform groundAnchor;
+	Transform downAnchor, rightAnchor, leftAnchor;
 	private bool jump;	
 	private int airJumps = 0;
 	public int airJumpsAllowed = 1;
 	// Use this for initialization
 	void Start () {
-		groundAnchor = transform.Find ("ground");
+		downAnchor = transform.Find ("down");
+		rightAnchor = transform.Find ("right");
+		leftAnchor = transform.Find ("left");
 	}
 
-	bool isOnGround() {
-		return Physics.Linecast (transform.position, groundAnchor.position, 1 << LayerMask.NameToLayer("floor")); //todo: mask to certain layers if necessary
+	bool isAgainstLevel(Transform direction) {
+		return Physics.Linecast (transform.position, direction.position, 1 << LayerMask.NameToLayer ("levelGeometry"));
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		jump = Input.GetButtonUp ("Jump");
@@ -30,16 +32,25 @@ public class Controls : MonoBehaviour {
 	void FixedUpdate() {
 		float hInput = Input.GetAxis ("Horizontal");
 
+		// prevent wall hang 
+		if (hInput > 0 && isAgainstLevel (rightAnchor) ||
+		    hInput < 0 && isAgainstLevel (leftAnchor)) {
+			hInput *= 0F;
+		}
+
+		// left right movement
 		if (hInput * GetComponent<Rigidbody> ().velocity.x < maxSpeed) {
 			GetComponent<Rigidbody>().AddForce (Vector2.right * hInput * moveForce);
 		}
 
+		// clamp to max speed
 		if (Mathf.Abs (GetComponent<Rigidbody> ().velocity.x) > maxSpeed) {
 			GetComponent<Rigidbody> ().velocity = new Vector2 (Mathf.Sign (GetComponent<Rigidbody> ().velocity.x) * maxSpeed, GetComponent<Rigidbody>().velocity.y);
 		}
 
+		// limit jumping to number of air jumps 
 		if (jump) { 
-			if (isOnGround()) {
+			if (isAgainstLevel(downAnchor)) {
 				addJumpForce();
 				airJumps = 0;
 			} else if(airJumps < airJumpsAllowed) {
