7d5963173d6817ab534d97ed3202fafe2871b3b3
Chris Madge <chris@madg.es>
2015-11-08 21:54:38 +0000

    added a scene to test an idea for the shooting mechanism (shootingtest)

---
+++ time-fighters/Assets/Aim.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aim : MonoBehaviour {
+
+	Vector3 mousePosition;
+	Vector3 objectPosition;
+	GameObject projectile;
+	Transform muzzle;
+
+	// Use this for initialization
+	void Start () {
+		muzzle = transform.FindChild ("muzzle");
+		projectile = Resources.Load ("aimtest/Projectile") as GameObject;
+	}
+
+	void Fire() {
+
+		Instantiate (projectile, muzzle.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePosition = Input.mousePosition;
+		objectPosition = Camera.main.WorldToScreenPoint (transform.position);
+		Vector3 result;
+		result = mousePosition - objectPosition;
+		float angle = Mathf.Atan2 (result.y, result.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler (0, 0, angle);
+		if (Input.GetMouseButtonDown (0)) {
+			Fire ();
+		}
+	}
+}
---
+++ time-fighters/Assets/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	public float bulletForce = 600f;
+	public float cleanUpAfter = 2f; //destroy object after n seconds
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.AddForce (transform.right*bulletForce);
+		StartCoroutine ("CleanUp");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator CleanUp() {
+		yield return new WaitForSeconds(cleanUpAfter);
+		Destroy (this);
+	}
+}
