8659c55637509a13ea179a045be5dc0db4d441fb
Dino Ratcliffe <yo@dino.io>
2015-11-11 10:14:31 +0000

    improved collision and added bouncing bullets

--- time-fighters/Assets/Scritps/PhisicalObject.cs
+++ time-fighters/Assets/Scritps/PhisicalObject.cs
@@ -6,6 +6,8 @@ public class PhisicalObject : MonoBehaviour {
 	public float drag = 0.1F;
 	public float maxVelocity = 0.5F;
 	public float timeScale = 1F;
+	public bool collision = true;
+	public bool noClip = false;
 
 	public Vector3 trajectory;
 
@@ -13,7 +15,21 @@ public class PhisicalObject : MonoBehaviour {
 
 	}
 
+	bool isAgainstLevel(Transform direction) {
+		return Physics.Linecast (transform.position, direction.position, 1 << LayerMask.NameToLayer ("levelGeometry"));
+	}
+
 	void FixedUpdate () {
+		RaycastHit hit = new RaycastHit ();
+		if (Physics.Linecast (transform.position, transform.position + ((trajectory * -1) * drag * timeScale) + (gravity * timeScale) + (trajectory * timeScale), out hit)) {
+			PhisicalObject obj = hit.collider.GetComponent<PhisicalObject>();
+			if ((!obj || obj.collision) && !noClip) {
+				trajectory =  trajectory - 2 * Vector3.Dot(trajectory, hit.normal) * (hit.normal);
+				transform.position = hit.point;
+				hit.collider.GetComponent<PhisicalObject>();
+			} 
+		}
+
 		AddForce ((trajectory * -1) * drag * timeScale);
 		transform.position += gravity * timeScale;
 		transform.position += trajectory * timeScale;
