747446f40b0135547bcfd7c34c1805cf11337067
Dino Ratcliffe <yo@dino.io>
2015-11-12 11:52:12 +0000

    adjusted player size code and adjusted box colliders for level

--- time-fighters/Assets/Scritps/BodyBuilder.cs
+++ time-fighters/Assets/Scritps/BodyBuilder.cs
@@ -11,16 +11,16 @@ public class BodyBuilder : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		BoxCollider boxCollider = gameObject.GetComponentInParent<BoxCollider> ();
-		gameObject.transform.localScale = new Vector3(boxCollider.size.x/height, boxCollider.size.y/width, boxCollider.size.z/depth);
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
+		gameObject.transform.localScale = new Vector3(boxCollider.size.x/width, boxCollider.size.y/height, boxCollider.size.z/depth);
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
 				for (int r = 0; r < depth; r++) {
 					GameObject g = (GameObject) Instantiate(item, transform.position, transform.rotation);
 					g.GetComponent<Renderer>().material.color = color;
 					g.GetComponent<Renderer>().material.SetColor("_EmissionColor", color);
 					g.GetComponentInChildren<Light>().color = color;
 					g.transform.parent = transform;
-					g.transform.localPosition = new Vector3(i-height/2, j-width/2, r-depth/2);
+					g.transform.localPosition = new Vector3(i-(width/2), j-(height/2), r-(depth/2));
 					g.transform.localScale = new Vector3(1F, 1F, 1F);
 				}
 			}
